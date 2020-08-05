d66276c19e1e27c05c3c427b4ff996c6c8dfb030
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-08-06 10:07:54 +1200

    Completed task 2

---
+++ src/rectangle/rectangle.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(500, 500);
+  }
+  
+  function draw() {
+    rect(30, 20, 200, 80, 10); // Create a rectangle
+    stroke("red"); // Stroke/outline colour of red
+    strokeWeight(6); // Stroke weight of 6
+  }
+  
